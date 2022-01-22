8378c62fdade4a075e667c753a3e799dc48abc07
TasfiaIslam <tasf1a027@gmail.com>
2022-01-23 00:25:23 +0600

    gatsby and sanity setup

---
+++ studio/schemas/project.js
@@ -0,0 +1,18 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    // ... other fields ...
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+  ],
+};
