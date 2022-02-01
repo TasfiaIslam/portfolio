aacd02b5a5cb9632b1d606444670905db5105263
TasfiaIslam <tasf1a027@gmail.com>
2022-02-01 16:45:51 +0600

    add schemas

--- studio/schemas/project.js
+++ studio/schemas/project.js
@@ -1,18 +1,46 @@
+// schemas are basically objects
 export default {
+  // The identifier for this document type used in the api's
   name: "project",
-  title: "Project",
+
+  // This is the display name for the type
+  title: "Projects",
+
+  // Schema type of Document
   type: "document",
+
   fields: [
-    // ... other fields ...
     {
       name: "title",
-      title: "Title",
+      title: "Project Title",
       type: "string",
+      description: "title of the project",
     },
     {
-      name: "description",
-      title: "Description",
-      type: "string",
+      name: "techStack",
+      title: "Technology Stack",
+      type: "array",
+      description: "languages and frameworks",
+      of: [
+        {
+          type: "reference",
+          to: [
+            {
+              type: "technology",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "createDate",
+      title: "Created At",
+      type: "date",
+      options: {
+        dateFormat: "YYYY-MM-DD",
+        calendarTodayLabel: "Today",
+      },
+      description: "project creation date",
     },
   ],
 };
---
+++ studio/schemas/technology.js
@@ -0,0 +1,13 @@
+export default {
+  name: "technology",
+  title: "Technologies",
+  type: "document",
+  fields: [
+    {
+      name: "technology",
+      title: "Project technology",
+      type: "string",
+      description: "languages and frameworks",
+    },
+  ],
+};
